8eaa5d3eb654ad176e6c8fe27378583b01cae5d7
G Martin <georgemartin1603@yahoo.com>
2021-03-13 02:12:36 -0600

    inital

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+function App() {
+  return (
+    <Container className="App">
+      
+    </Container>
+  );
+}
+
+export default App;
+
+const Container = styled.div`
+  
+`
