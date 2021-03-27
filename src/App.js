418153c7da296cce04f0492993ef43ef1d0d3a82
G Martin <georgemartin1603@yahoo.com>
2021-03-27 00:07:11 -0500

    Add expense and option working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import {labels, seeds, chemicals} from './data'
+import {getData, writeData} from './data'
 
 function App() {
 
+  const [labels, setLabels] = useState('')
+  const [expenses, setExpenses] = useState('')
+  const [newOption, setNewOption] = useState('')
   const [seed, setSeed] = useState('')
   const [qty, setQty] = useState('')
   const [price, setPrice] = useState('')
@@ -11,80 +14,147 @@ function App() {
   const [landLord, setLandLord] = useState('')
   const [crop, setCrop] = useState('')
   const [chemical, setChemical] = useState('')
+  const [fertilizer, setFertilizer] = useState('')
   const [seedList, setSeedList] = useState([])
   const [chemList, setChemList] = useState([])
+  const [fuelList, setFuelList] = useState([])
+  const [fertList, setFertList] = useState([])
+  const [truckingList, setTruckingList] = useState([])
+  const [optionList, setOptionList] = useState([])
+  const [expense, setExpense] = useState('')
   const [id, setId] = useState(1)
+  const [add, setAdd] = useState(false)
+  const [newExpense, setNewExpense] = useState('')
 
   
 
+  useEffect(() => {
+    getData('labels', setLabels)
+    getData('expenses', setExpenses)
+  },[])
+
+  const removeItem = (arr, attr, value, fun) => {
+    console.log(arr)
+    let i = arr.length;
+    let array = []
+    while(i--){
+        if(arr[i] 
+           && arr[i].hasOwnProperty(attr) 
+           && arr[i][attr] === value){ 
+            // console.log(arr)
+            // arr.splice(i,1)
+            // console.log(arr)
+          }else{
+            array.splice(0, 0, arr[i])
+            // array.push(arr[i])
+          }      
+    }
+    fun(array)
+  }
+
+  const addOption = (arr, attr, value) => {
+    let i = arr.length
+    while (i--) {
+      if(arr[i] 
+        && arr[i].hasOwnProperty(attr) 
+        && arr[i][attr] === value){ 
+         arr[i]["options"].push(newOption)
+         console.log(arr)
+         return (
+           setExpenses(arr)
+         )
+       }
+    }
+  } 
   
-  const addSeed = (e) => {
+  const addNewExpense = (e) => {
+    e.preventDefault()
+    if (newExpense) {
+
+      let obj = {
+        label: newExpense,
+        dispatch: 'SET_EXP',
+        options: []
+      }
+      writeData('expenses', obj, expenses)
+      document.getElementById('add__new').reset()
+    } else {
+      addOption(expenses, "label", expense)
+      console.log(expense)
+      // let array = optionList
+      // array.push(newOption)
+      // let obj = {
+      //   label: expense,
+      //   dispatch: 'SET_EXP',
+      //   options: array
+      // } 
+      writeData('expenses', expenses)
+      document.getElementById('add__new').reset()
+    }
+  } 
+
+
+  const addExpense = (e, exp) => {
     e.preventDefault()
     if (price > 0 && qty > 0) {
+      let obj = {}
       let int = id + 1
       setId(int)
-      let obj = {
-        name: seed,
-        price: price,
-        qty: qty,
-        id: int
+      switch (exp) {
+        case ('Seed'):
+          obj = {
+            name: seed,
+            price: price,
+            qty: qty,
+            id: int
+            
+          }
+          setSeedList([...seedList, obj])
+          break;
+        case ('Chemical'):
+          obj = {
+            name: chemical,
+            price: price,
+            qty: qty,
+            id: id
+          }
+          setChemList([...chemList, obj])
+          break;
+        case ('Fertilizer'):
+          obj = {
+            name: fertilizer,
+            price: price,
+            qty: qty,
+            id: id
+          }
+          setFertList([...chemList, obj])
+          break;
+        case ('LP'):
+          obj = {
+            name: expense,
+            price: price,
+            qty: qty,
+            id: id
+          }
+          setFuelList([...fuelList, obj])
+          break;
+        case ('Trucking'):
+          obj = {
+            name: expense,
+            price: price,
+            qty: qty,
+            id: id
+          }
+          setTruckingList([...truckingList, obj])
+          break;
+            default:
+              return
       }
-      setSeedList([...seedList, obj])
       console.log(id)
+      document.getElementById("Expense").reset()
+      setOptionList('')
     }
     else return
-  }
-  const addChemical = (e) => {
-    e.preventDefault()
-    if (price > 0 && qty > 0) {
-      let int = id + 1
-      setId(int)
-      let obj = {
-        name: chemical,
-        price: price,
-        qty: qty,
-        id: id
-      }
-      setChemList([...chemList, obj])
-    }
-    else return
-  }
-
-  const removeSeed = (arr, attr, value) => {
-    
-    let i = arr.length;
-    let array = []
-    while(i--){
-        if(arr[i] 
-           && arr[i].hasOwnProperty(attr) 
-           && arr[i][attr] === value){ 
-            // console.log(arr)
-            // arr.splice(i,1)
-            // console.log(arr)
-          }else{
-            array.splice(0, 0, arr[i])
-            // array.push(arr[i])
-          }      
-    }
-    setSeedList(array)
-  }
-  const removeChemical = (arr, attr, value) => {
-    
-    let i = arr.length;
-    let array = []
-    while(i--){
-        if(arr[i] 
-           && arr[i].hasOwnProperty(attr) 
-           && arr[i][attr] === value){ 
-            // console.log(arr)
-            // arr.splice(i,1)
-            // console.log(arr)
-          }else{
-            array.splice(0, 0, arr[i])
-            // array.push(arr[i])
-          }      
-    }
-    setChemList(array)
   }
 
   const handleSubmit = (e) => {
@@ -95,7 +165,7 @@ function App() {
   }
 
   const handleChange = (e) => {
-    console.log(e.target.name)
+    
     switch (e.target.name) {
       case ("Land Lord") :
         setLandLord(e.target.value)
@@ -112,6 +182,18 @@ function App() {
       case ("Chemical") :
         setChemical(e.target.value)
         break
+      case ("Fertilizer") :
+        setFertilizer(e.target.value)
+        break
+      case ("Expense") :
+        setExpense(e.target.value)
+        break
+      case ("Add_Expense") :
+        e.target.value === 'add__expense' ?
+        setAdd(true)
+        :
+        setExpense(e.target.value)
+        break
       case ("Price") :
         setPrice(e.target.value)
         break
@@ -121,6 +203,20 @@ function App() {
       default:
         return
     }
+    expenses?.map((item) => {
+      if(item.label === e.target.value) {
+        let a = []
+        item.options ?
+        item.options.map((option) => {
+          a.splice(0, 0, option)
+          return(
+            setOptionList(a)
+          )
+        })
+        :
+        setOptionList('')
+      } else return null
+    })
   }
 
   return (
@@ -132,9 +228,9 @@ function App() {
           labels &&
           labels.map((obj) => (
             <Select>
-              <label for={obj.label}>{obj.label}</label>
+              <label htmlFor={obj.label}>{obj.label}</label>
               <select name={obj.label} onChange={(e) => handleChange(e)}>
-              <option value="" selected hidden >Choose here</option>
+              <option value="" defaultValue hidden >Choose here</option>
                 {
                   obj.options && 
                   obj.options.map((option) => (
@@ -146,52 +242,93 @@ function App() {
           ))
         }
       </form>
-      <form action="add seed" >
+      <form id="Expense" action="add seed" >
         <Select>
+        <label htmlFor="Expense">Add Expense</label>
+        <select name="Expense"  onChange={(e) => handleChange(e)}>
+          <option value="" defaultValue hidden>Choose here</option>
 
-        <label for={seeds.label}>{seeds.label}</label>
-        <select name={seeds.label} onChange={(e) => handleChange(e)}>
-          <option value="" selected hidden>Choose here</option>
           {
-            seeds.options.map((obj) => (
-              <option>{obj}</option>
-              ))
+            expenses &&
+            expenses.map((obj) => (
+              <option key={obj.label}value={obj.label}> {obj.label} </option>
+            ))
           }
         </select>
-          <label For="qty">Quantity</label>
-          <input placeholder="QTY" name="Qty"type="number" onChange={(e) => handleChange(e)}/>
-          <label For="price">Price Per Bag</label>
-          <input placeholder="Price" name="Price"type="number" onChange={(e) => handleChange(e)}/>
-          <button type="submit" onClick={(e) => addSeed(e)}>ADD</button>
-        </Select>
-      </form>
-      <form action="add chem" >
-        <Select>
-
-        <label for={chemicals.label}>{chemicals.label}</label>
-        <select name={chemicals.label} onChange={(e) => handleChange(e)}>
-          <option value="" selected hidden>Choose here</option>
-          {
-            chemicals.options.map((obj) => (
-              <option>{obj}</option>
-              ))
+        {
+          optionList.length > 0 ?  
+           
+          // <label for="expense">Add Expense</label>
+          <select name={expense} onChange={(e) => handleChange(e)}>
+            <option value="" defaulfValue hidden>Choose here</option>
+            
+            {        
+              optionList &&
+              optionList.map((obj) => (
+                <option key={obj} >{obj}</option>
+              ))         
             }
-        </select>
-          <label For="qty">Quantity</label>
+          </select>
+          :
+          ''
+        }
+          <label htmlFor="qty">Quantity</label>
           <input placeholder="QTY" name="Qty"type="number" onChange={(e) => handleChange(e)}/>
-          <label For="price">Price Per Bag</label>
+          <label htmlFor="price">Price Per Unit</label>
           <input placeholder="Price" name="Price"type="number" onChange={(e) => handleChange(e)}/>
-          <button type="submit" onClick={(e) => addChemical(e)}>ADD</button>
+          <button type="submit" onClick={(e) => addExpense(e, expense)}>ADD</button>
         </Select>
       </form>
-        
+      <form id='add__new' action="add">
+      <Select>
+        <label htmlFor="Add_Expense">Add New Expense</label>
+        <select name="Add_Expense"  onChange={(e) => handleChange(e)}>
+          <option value="" defaultValue hidden>Choose here</option>
 
+          {
+            expenses &&
+            expenses.map((obj) => (
+              <option key={obj.label}value={obj.label}> {obj.label} </option>
+            ))
+          }
+          <option value="add__expense">Add New</option>
+        </select>
+        {
+          // optionList ?  
+           
+          // // <label for="expense">Add Expense</label>
+          // <select name={expense} onChange={(e) => {handleChange(e); document.getElementById('add__new__input').focus()}}>
+          //   <option value="" selected hidden>Choose here</option>
+            
+          //   {        
+          //     optionList &&
+          //     optionList.map((obj) => (
+          //       <option key={obj} >{obj}</option>
+          //     ))         
+          //   }
+          // </select>
+          // :
+          add ?
+          <input id='add__new__input' type="text"onChange= {(e) => setNewExpense(e.target.value)}/>
+          :
+          expense ? 
+          <input type="text" onChange={(e) => setNewOption(e.target.value)}/>
+          : ''
+            
+        }  
+          <button type="submit" onClick={(e) => addNewExpense(e)}>ADD</button>
+        </Select>
+      </form>
       </Input>
     <Report>
       <Header>
         <h2>{landLord} {crop} {year}</h2>
       </Header>
+      {
+        seedList.length > 0 ?
         <h3>Seed</h3>
+        : ''
+      }
       <Expense>
         {
           seedList &&
@@ -199,13 +336,17 @@ function App() {
             <Item>
             <p><b>Name:</b> {obj.name}</p>
             <p><b>Price per bag:</b> {obj.price}</p>
-            <p><b>Total:</b> {obj.price * obj.qty}</p>
-            <button onClick={() => removeSeed(seedList, 'id', obj.id)}>Delete</button>
+            <p><b>Total:</b> ${obj.price * obj.qty}</p>
+            <button onClick={() => removeItem(seedList, 'id', obj.id, setSeedList)}>Delete</button>
             </Item>
           ))
         }
       </Expense>
+      {
+        chemList.length > 0 ?
         <h3>Chemicals</h3>
+        : ''
+      }
       <Expense>
         {
           chemList &&
@@ -214,7 +355,61 @@ function App() {
             <p><b>Name:</b> {obj.name}</p>
             <p><b>Price per gal:</b> ${obj.price}</p>
             <p><b>Total:</b> ${obj.price * obj.qty}</p>
-            <button onClick={() => removeChemical(chemList, 'id', obj.id)}>Delete</button>
+            <button onClick={() => removeItem(chemList, 'id', obj.id, setChemList)}>Delete</button>
+            </Item>
+          ))
+        }
+      </Expense>
+      {
+        fertList.length > 0 ?
+        <h3>Fertilizer</h3>
+        : ''
+      }
+      <Expense>
+        {
+          fertList &&
+          fertList.map((obj) => (
+            <Item>
+            <p><b>Name:</b> {obj.name}</p>
+            <p><b>Price per gal:</b> ${obj.price}</p>
+            <p><b>Total:</b> ${obj.price * obj.qty}</p>
+            <button onClick={() => removeItem(chemList, 'id', obj.id, setFertList)}>Delete</button>
+            </Item>
+          ))
+        }
+      </Expense>
+      {
+        fuelList.length > 0 ?
+        <h3>Fuel</h3>
+        : ''
+      }
+      <Expense>
+        {
+          fuelList &&
+          fuelList.map((obj) => (
+            <Item>
+            <p><b>Name:</b> {obj.name}</p>
+            <p><b>Price per gal:</b> ${obj.price}</p>
+            <p><b>Total:</b> ${obj.price * obj.qty}</p>
+            <button onClick={() => removeItem(chemList, 'id', obj.id, setFuelList)}>Delete</button>
+            </Item>
+          ))
+        }
+      </Expense>
+      {
+        truckingList.length > 0 ?
+        <h3>Trucking</h3>
+        : ''
+      }
+      <Expense>
+        {
+          truckingList &&
+          truckingList.map((obj) => (
+            <Item>
+            <p><b>Name:</b> {obj.name}</p>
+            <p><b>Price per Bu:</b> ${obj.price}</p>
+            <p><b>Total:</b> ${obj.price * obj.qty}</p>
+            <button onClick={() => removeItem(truckingList, 'id', obj.id, setTruckingList)}>Delete</button>
             </Item>
           ))
         }
@@ -250,7 +445,7 @@ const Header = styled.div`
 `
 const Expense = styled.div`
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: column;
 
 `
 const Item = styled.div`
--- src/data.js
+++ src/data.js
@@ -1,17 +1,62 @@
-export const labels = [
-{label:'Land Lord', dispatch:'SET_LAND_LORD', options: ['Elane', 'Katheen', 'George']}, 
-{label:'Crop', dispatch:'SET_CROP', options: ['Corn', 'Soy Beans']}, 
-{label:'Year', dispatch:'SET_YEAR', options: ['2018', '2019', '2020', '2021']}, 
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCelAccvshw9mbBgJ_T55yao6tBuy7zLVo",
+    authDomain: "farm-report-86ac2.firebaseapp.com",
+    projectId: "farm-report-86ac2",
+    storageBucket: "farm-report-86ac2.appspot.com",
+    messagingSenderId: "1035453646204",
+    appId: "1:1035453646204:web:0afc537ef2d4c3e36410f9",
+    measurementId: "G-2MB2WPLY00"
+  };
+
+firebase.initializeApp(firebaseConfig)
+
+export const db = firebase.firestore();
+
+export const getData = (coll, func) => {
+    db.collection(coll).get()
+    .then((snapshot) => {
+        snapshot.forEach((docs) => {
+            switch (coll) {
+                case "labels":
+                    func(docs.data().labels)
+                    console.log(docs.data().labels)
+                    break
+                case "expenses":
+                    func(docs.data().expenses)
+                    console.log(docs.data().expenses)
+                    break
+                default:
+                    return
+            }
+        })
+    })
+}
+
+export const writeData = (coll, exp, expenses) => {
+    console.log(expenses)
+    if (exp) {
+        let array = expenses
+        array.push(exp)
+    }
+    db.collection(coll).doc('8CJ5sBDg05YAVjow0HUW').set({
+        expenses
+        
+    })
+}
+
+// export let labels = getData('labels')
+// export const labels = [
+// {label:'Land Lord', dispatch:'SET_LAND_LORD', options: ['Elane', 'Katheen', 'George']}, 
+// {label:'Crop', dispatch:'SET_CROP', options: ['Corn', 'Soy Beans']}, 
+// {label:'Year', dispatch:'SET_YEAR', options: ['2018', '2019', '2020', '2021']}, 
+// ]
+
+export const expenses = [
+    {label:'Seed', dispatch:'SET_SEED', options: ['PI45732', 'PI21832', 'PI45748', 'PI45735',]},
+    {label:'Chemical', dispatch:'SET_CHEMICAL', options: ['Roundup', '24D', 'Chemical C', 'Chemical D',]},
+    {label:'Fertilizer', dispatch:'SET_CHEMICAL', options: ['liquid 28-0-0', 'Dry 0-0-60', 'Avail', 'N-Serve',]},
+    {label:'LP', dispatch:'SET_CHEMICAL'},
+    {label:'Trucking', dispatch:'SET_CHEMICAL'},
 ]
-
-export const seeds = 
-{label:'Seed', dispatch:'SET_SEED', options: ['PI45732', 'PI21832', 'PI45748', 'PI45735',]}
-export const chemicals =
-{label:'Chemical', dispatch:'SET_CHEMICAL', options: ['Roundup', '24D', 'Chemical C', 'Chemical D',]}
-// {label:'Seed Price', dispatch:'SET_PRICE', options: [250, 260, 270, 280,]}
- 
-// {label:'Chem Price', dispatch:'SET_CHEM_PRICE', options: [15, 8, 19, 10,]}, 
-
-
-export const landLord = 
-{label:'Land Lord', dispatch:'SET_LAND_LORD', options: ['Elane', 'Katheen', 'George']}
